13017ce65ebd7b753b4f4fe3cc81ed5c16d90aef
Sarah Elashmawy <74767392+sarahelashmawy@users.noreply.github.com>
2022-09-14 09:27:49 +0000

    ietjes slimmer gemaakt

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import random 
-from bke import EvaluationAgent, start
+from bke import EvaluationAgent, start, can_win
 
-class MyRandomagent (EvaluationAgent):
-  def evaluate(self, board, my_symbol, oppponent_symbol): 
-    return random.randint(1,500)
+class MijnSpeler (EvaluationAgent):
+  def evaluate(self, board, my_symbol, opponent_symbol):
+    getal = 1
+    if can_win(board, opponent_symbol):
+      getal = getal - 1000
+    return getal
 
-my_random_agent = MyRandomagent()
-start(player_o=my_random_agent, player_x=my_random_agent)
+mijn_speler = MijnSpeler()
+start(player_o=mijn_speler)
