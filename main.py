f386a0bec61bef320d5889f38049a3a83729322c
Sarah Elashmawy <74767392+sarahelashmawy@users.noreply.github.com>
2022-09-21 09:53:45 +0000

    basic agent zonder training

--- main.py
+++ main.py
@@ -1,44 +1,19 @@
-import random
-from bke import EvaluationAgent, OptimalEvaluationAgent, can_win, start
+from bke import MLAgent, is_winner, opponent, start
 
-#twee tegenstanders
-start()
+class MyAgent(MLAgent):
+ def evaluate(self, board):
+   if is_winner(board, self.symbol):
+     reward = 1 
 
-#dom speler 
-class MyRandomAgent(EvaluationAgent):
-  def evaluate(self, board, my_symbol, opponent_symbol):
-    return random.randint(1,500)
+   elif is_winner(board, opponent[self.symbol]):
+     reward = -1
 
-my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent)
+   else:
+     reward = 0
 
-#ietjes slimmer speler
-class MijnSpeler(EvaluationAgent):
-  def evaluate(self, board, my_symbol, opponent_symbol):
-    getal = 1
-    if can_win(board, opponent_symbol):
-      getal = getal - 1000
-      return getal
+   return reward
 
-mijn_speler = MijnSpeler()
-start(plyer_o=mijn-speler)
-
-class MedAgent(OptimalEvaluationAgent):
-  def evaluate(self, board, my_symbol, opponent_symbol):
-    
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
+my_agent = MyAgent()
+start(player_o=my_agent)
+   
 
