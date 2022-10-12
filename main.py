95040d9def55d754efc5460517000f12067c407a
Sarah Elashmawy <74767392+sarahelashmawy@users.noreply.github.com>
2022-10-12 10:37:20 +0000

    Spel opnieuw spelen toegevoegd

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import random
 from bke import EvaluationAgent, MLAgent, RandomAgent, train_and_plot, is_winner, opponent, train, load, save, start
 
+class MyRandomAgent(EvaluationAgent):
+  def evaluate(self, board, my_symbol, opponent_symbol):
+    return random.randint(1,500)
 
+
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+
+
+my_random_agent = MyRandomAgent()
+  
+my_agent = MyAgent(alpha=0.9, epsilon=0.2)
+my_agent = load('MyAgent_6000')
+my_agent.learning = True
+  
+random.seed(1)
+random_agent = RandomAgent()
+
+  
 def main():
   print("\n" 
   "Boter, kaas en eieren! \n")
@@ -14,22 +39,24 @@ def main():
   
   y = input("Kies van de genoemde opties: ")
   
-  while True:
-    if y == "1":
-      games()
-    elif y == "2":
+  if y == "1":
+    menu()
+  elif y == "2":
+    while True:
       train_and_plot(
         agent=my_agent,
         validation_agent=random_agent,
         iterations=50,
         trainings=500,
         validations=1000)
-    else:
-      print("\n"
-      "Input is fout. Voer een geldig keuze in.\n")
-      y = input("Kies van de genoemde opties: ")
-  
-def games():
+  else:
+    print("\n"
+          "Input is fout. Voer een geldig keuze in.\n")
+    main()
+
+
+
+def menu():
 #menu van welke spellen 
   print("\n"
   "1. 2 spelers \n"
@@ -39,48 +66,67 @@ def games():
 
   x = input("Kies van de genoemde opties: ")
   
-  while True:
-    if x == "1": 
+  if x == "1": 
+    while True:
       print ("2 spelers \n")
       start()
-    elif x == "2":
+    
+      print("Wil je opnieuw spelen? \n"
+          "1. Ja \n"
+          "2. Nee \n"
+          "3. Terug naar menu. \n")
+      z = input("Keuze: ")
+      if z == "1":
+        continue  
+      elif z == "2":
+        print("Doei!")
+        break 
+      elif z =="3":
+        menu()  
+    
+  elif x == "2":
+    while True:
       print("Makkelijke level")
       start(player_o=my_random_agent)
-    elif x == "3":
+
+      print("Wil je opnieuw spelen? \n"
+          "1. Ja \n"
+          "2. Nee \n"
+          "3. Terug naar menu. \n")
+      z = input("Keuze: ")
+      if z == "1":
+        continue  
+      elif z == "2":
+        print("Doei!")
+        break
+      elif z =="3":
+        menu()
+      
+  elif x == "3":
+    while True:
       print("Moeilijke Level")
       start(player_o=my_agent)
-    elif x == "4":
-      main()
-    else:
-      print("\n"
-      "Input is fout. Voer een geldig keuze in.\n")
-      x = input("Kies van de genoemde opties: ")
+
+      print("Wil je opnieuw spelen? \n"
+          "1. Ja \n"
+          "2. Nee \n"
+          "3. Terug naar menu. \n")
+      z = input("Keuze: ")
+      
+      if z == "1":
+        continue  
+      elif z == "2":
+        print("Doei!")
+        break
+      elif z =="3":
+        menu()  
   
-
-class MyRandomAgent(EvaluationAgent):
-  def evaluate(self, board, my_symbol, opponent_symbol):
-    return random.randint(1,500)
-
-my_random_agent = MyRandomAgent()
-
-class MyAgent(MLAgent):
-    def evaluate(self, board):
-        if is_winner(board, self.symbol):
-            reward = 1
-        elif is_winner(board, opponent[self.symbol]):
-            reward = -1
-        else:
-            reward = 0
-        return reward
-
-my_agent = MyAgent(alpha=0.9, epsilon=0.2)
-
-my_agent = load('MyAgent_6000')
-my_agent.learning = True
-
-random.seed(1)
-random_agent = RandomAgent()
-
-
+  elif x == "4":
+    main()
+  else:
+    print("\n"
+    "Input is fout. Voer een geldig keuze in.\n")
+    menu()
+  
 main()
 
